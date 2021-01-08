1e9d970fcfe1221cb4fb843194e29ea5c49cb0f3
ShivaniSwaroop <51238523+ShivaniSwaroop@users.noreply.github.com>
2021-01-08 19:50:47 +0530

    Create ShivApp.java

---
+++ ShivApp.java
@@ -0,0 +1,9 @@
+package com.luv2code.springdemo;
+public class ShivApp
+{
+  public static void main(String[] args)
+  {
+    BbCoach tCoach = new BbCoach();
+    System.out.println(tCoach.getWorkout());
+  }
+}
