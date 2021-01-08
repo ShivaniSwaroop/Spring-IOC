33991de8f260ed38517bc515e0852834daf76c5d
ShivaniSwaroop <51238523+ShivaniSwaroop@users.noreply.github.com>
2021-01-08 19:57:09 +0530

    Update BbCoach.java

--- BbCoach.java
+++ BbCoach.java
@@ -1,6 +1,7 @@
 package com.luv2code.springdemo;
-public class BbCoach
+public class BbCoach implements Coach
 {
+  @Override
   public String getWorkout()
   {
     return "Have a good diet plan :)";
